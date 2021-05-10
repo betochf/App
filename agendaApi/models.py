4f84029bb404f1e1f97e47eda7f2feb494e70ba0
Alberto Chavez Falcon <alberto.chavez@ENCORA1s-MacBook-Pro.local>
2021-05-10 10:49:54 -0500

    procfile update

--- agendaApi/models.py
+++ agendaApi/models.py
@@ -7,8 +7,8 @@ class Contact(models.Model):
     name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     company = models.CharField(max_length=40)
-    phone = models.CharField(max_length=10)
-    email = models.EmailField()
+    phone = models.CharField(max_length=10, unique=True)
+    email = models.EmailField(unique=True)
 
     def __str__(self):
         return self.name
